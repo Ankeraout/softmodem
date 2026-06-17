6cfaba1c99c7252b73595adb7a2f6e84c6110262
Ankeraout <ankeraout@yahoo.com>
2026-06-17 19:50:34 +0200

    Removed old code

--- modem/util/byte_receiver.py
+++
@@ -1,8 +0,0 @@
-import modem
-
-class BufferByteReceiver(modem.IByteReceiver):
-    def __init__(self) -> None:
-        self.buffer = bytearray()
-
-    def receive_bytes(self, data: bytes):
-        self.buffer.extend(data)
